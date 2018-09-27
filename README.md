715e1d22b7e4538b32a4efdd8e69b7f7c05e4881
liipx <lipengxiang_@outlook.com>
2018-09-28 01:07:24 +0800

    support soar

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # SublimeLinter-soar
+=================================================
 SublimeLinter plugin for SQL by SOAR.
+
+## Installation
+SublimeLinter 3 must be installed in order to use this plugin. If SublimeLinter 3 is not installed, please follow the instructions [here](http://sublimelinter.readthedocs.org/en/latest/installation.html).
+
+### Linter installation
+Before using this plugin, you must ensure that `SOAR` is installed on your system. To install `SOAR`, do the following:
+
+```
+// TODO
+```
+
+### Plugin installation
+Open Sublim Text and click `Preference -> Package Settings -> SublimeLinter -> Settings`, set the config as following:
+
+```
+// SublimeLinter Settings - User
+{
+	"linters": {
+        "soar": {
+            "soar_path":"you_soar_path"
+        },
+    },
+}
+
+```
+
+Now, open a sql file and enjoy.
--- linter.py
+++ linter.py
@@ -1,25 +1,29 @@
-from SublimeLinter.lint import Linter, util  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+from SublimeLinter.lint import Linter, util, persist  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
 
 class Soar(Linter):
-    cmd = 'soar -report-type lint -query'
+    try:
+        cmd = persist.settings.get('linters').get('soar').get("soar_path") + " -report-type lint -query"
+    except Exception as e:
+        cmd = "soar -report-type lint -query"
+    
     multiline = True
+    
     tempfile_suffix = 'sql'
     regex = (
+        r'^.+?:(?P<line>\d+):'
         r'(?P<message>.+$(?:\r?\n  .+$)*)'
     )
 
-
-    line_col_base = (1, 1)
-    output_stream = util.STREAM_BOTH
     defaults = {
         'selector': 'source.sql'
     }
 
+    line_col_base = (1, 1)
+    output_stream = util.STREAM_BOTH
 
     def split_match(self, match):
         """Process each match modifying or discarding it."""
         match, line, col, error, warning, message, near = super().split_match(match)
         message = message.split(":")[-1]
-        line = 1
-        print(match, line, col, error, warning, message, near)
+        col=0
         return  match, line, col, error, warning, message, near
